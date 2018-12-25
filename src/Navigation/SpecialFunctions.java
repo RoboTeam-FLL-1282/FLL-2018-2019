3568d9d09c57ea14af73f3715304629068f0acb5
RoboTeam <44878143+RoboTeam-FLL-1282@users.noreply.github.com>
2018-12-25 15:59:50 +0200

    Update SpecialFunctions.java

--- src/Navigation/SpecialFunctions.java
+++ src/Navigation/SpecialFunctions.java
@@ -4,6 +4,7 @@ import EV3.BrickButtons;
 import EV3.MoveTank;
 import EV3.Ports;
 import EV3.Wait;
+import Motion.Accelerator;
 import Motion.Aligner;
 import Motion.GyroPID;
 import Motion.LineAlignment;
@@ -22,13 +23,14 @@ public class SpecialFunctions {
 		pid.g.recalibrate();
 
 		// Move robot
+		Accelerator.accelerate(2, -10, -100, false);
 		pid.startPID();
-		Wait.time(1300);
+		Wait.time(400);
 		pid.stopPID();
 
 		// Align on white line and then turn x degrees
 		//		Display.resetScreen();
-		WhiteLineAlignment.align(-100);
+		WhiteLineAlignment.align(-250);
 		Sound.beep();
 		pid.g.reset();
 		MoveTank.onForCent(200, 200, 480, true);
