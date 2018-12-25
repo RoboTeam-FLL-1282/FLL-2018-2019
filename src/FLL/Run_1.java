23944d6b84e83a903c3ccd596bf5d0c533cb3790
RoboTeam <44878143+RoboTeam-FLL-1282@users.noreply.github.com>
2018-12-25 15:59:15 +0200

    Update Run_1.java

--- src/FLL/Run_1.java
+++ src/FLL/Run_1.java
@@ -1,8 +1,11 @@
 package FLL;
 
-import EV3.Wait;
+import EV3.MoveTank;
+import EV3.Sound;
+import Motion.Accelerator;
 import Motion.GyroPID;
 import Navigation.SpecialFunctions;
+import Navigation.Traveler;
 import Tools.Default;
 
 public class Run_1 {
@@ -16,14 +19,17 @@ public class Run_1 {
 		Default.settings();
 				
 		GyroPID pid = SpecialFunctions.navigateToOpsiteSection();
-			
-		pid.g.reset();
-		pid.setTarget(90);
+					
+		Sound.beep(100);
+		MoveTank.onForCent(-100, -100, 20, true);
+
+		Traveler t = new Traveler(0, 0, 15, 8);
 		
-		pid.startPID();
-		Wait.time(2000);
-		pid.stopPID();
+		Sound.beep(100);
+		t.turnInSpot(100, -100);
 		
+		Sound.beep(100);
+		MoveTank.onForCent(-100, -100, 300, true);
 		
 		pid.closePID();
 
