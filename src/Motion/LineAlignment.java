5f7c6d8703609dee217492f2e8dc7fd9156fd67b
RoboTeam <44878143+RoboTeam-FLL-1282@users.noreply.github.com>
2018-12-25 14:21:10 +0200

    Add files via upload

---
+++ src/Motion/LineAlignment.java
@@ -0,0 +1,59 @@
+package Motion;
+
+import EV3.MoveTank;
+
+public class LineAlignment {
+
+	public static void align(int speed) {
+				
+		int turnSpeed = speed-speed/2;
+		
+		Sides side = BlackLineAlignment.find(speed);
+		
+		turnToBlack(turnSpeed, side);
+		
+		side = WhiteLineAlignment.find(speed);
+		
+		turnToWhite(turnSpeed, side);
+	}
+	
+	public static void turnToBlack(int turnSpeed, Sides side) {
+		while(precision(Aligner.leftSensor.reflectedLight(),4) != precision(Aligner.rightSensor.reflectedLight(), 4) 
+				|| precision(Aligner.rightSensor.reflectedLight(), 1) >= 0.4) {
+			
+			if(side == Sides.LEFT) {
+				MoveTank.on(turnSpeed, -1*turnSpeed);
+			}
+			
+			else {
+				MoveTank.on(-1*turnSpeed, turnSpeed);
+			}
+			
+		}
+		
+		MoveTank.off();
+	}
+	
+	public static void turnToWhite(int turnSpeed, Sides side) {
+		while(precision(Aligner.leftSensor.reflectedLight(),4) != precision(Aligner.rightSensor.reflectedLight(), 4) 
+				/*|| precision(Aligner.rightSensor.reflectedLight(), 1) <= 0.5*/) {
+			
+			if(side == Sides.LEFT) {
+				MoveTank.on(turnSpeed, -1*turnSpeed);
+			}
+			
+			else {
+				MoveTank.on(-1*turnSpeed, turnSpeed);
+			}
+			
+		}
+		
+		MoveTank.off();
+	}
+	
+	public static double precision(double number, int precision) {
+		double mul = Math.pow(10, precision);
+		return ((int)(number*mul))/mul;
+	}
+	
+}
